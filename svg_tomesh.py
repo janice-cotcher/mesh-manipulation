7ebdfe66400510b4c5e314ffc7bc3df8a8c681f1
Janice Cotcher <42756980+janice-cotcher@users.noreply.github.com>
2026-06-04 02:17:41 +0000

    print points

--- svg_tomesh.py
+++ svg_tomesh.py
@@ -4,17 +4,9 @@ import meshlib.mrmeshpy as mrmeshpy
 polyline_path = "durins_door.svg"
 polylines = mrmeshpy.LinesLoad.fromSvg(polyline_path)
 contours = polylines.contours()
-
-
-# 2. Triangulate the contours to create a flat mesh
-# By default, this generates a Mesh object from the 2D closed paths
-# # Create an empty bit set for faces
-# faces = mrmeshpy.FaceBitSet()
-# faces.setAll(True)
-
-# # Extrude the 2D mesh into a 3D solid block
-# # distance controls how thick the 3D extrusion will be ($thickness$)
-# extruded_mesh = mrmeshpy.offsetMesh(mesh, distance=5.0)
-
-# # 3. Export the resulting mesh (e.g., to an STL or OBJ file)
-# mrmeshpy.saveMesh(extruded_mesh, "output_mesh.stl")
+count = 0
+for contour in contours:
+    # print(type(contour))
+    for item in contour:
+        for point in item:
+            print(point)
